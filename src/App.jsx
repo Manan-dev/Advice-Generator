1b403f85b59baaa4431d201a21fb62496ad6ad67
manan-dev <mananpatel6902@gmail.com>
2022-08-21 17:46:49 -0400

    Added random advice functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,29 @@ import { FaDiceFive } from 'react-icons/fa';
 function App() {
 	const [advice, setAdvice] = useState('');
 
-	useEffect(() => {
-		const getAdvice = async () => {
-			try {
-				const response = await fetch(
-					'https://api.adviceslip.com/advice'
-				);
-				const data = await response.json();
-				setAdvice(data.slip);
-			} catch (err) {
-				console.log(err.message);
-			}
-		};
-		getAdvice();
-	}, []);
+	const getAdvice = async () => {
+		try {
+			const response = await fetch('https://api.adviceslip.com/advice');
+			const data = await response.json();
+			setAdvice(data.slip);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
 
 	return (
 		<div className="App">
 			<div className="container">
 				<div className="heading">
-					<p>Advice #{advice.id}</p>
-					{/* <p>Advice #117</p> */}
+					<p>Advice #{advice ? advice.id : 1}</p>
 				</div>
-				<div className="advice">"{advice.advice}"</div>
-				{/* <div className="advice">
-					"It is easy to sit up and take notice, what's difficult is
-					getting up and taking action."
-				</div> */}
+				<div className="advice">
+					"
+					{advice
+						? advice.advice
+						: "It is easy to sit up and take notice, what's difficult is getting up and taking action"}
+					"
+				</div>
 				<table className="table">
 					<tbody>
 						<tr>
@@ -47,7 +43,7 @@ function App() {
 						</tr>
 					</tbody>
 				</table>
-				<div className="footer">
+				<div className="footer" onClick={getAdvice}>
 					<div className="dice">
 						<FaDiceFive />
 					</div>
